e656d9f6b01ff794c509667915032b9099f3328e
Ian sun <lauer3912@gmail.com>
2017-07-15 17:18:45 +0800

    2017-07-15 17:18:45 dwowan gif update [fileChanges=2] [fileAdd=0]

--- spider/dwowan-gif-spider/dwowan_gif_spider/pipelines.py
+++ spider/dwowan-gif-spider/dwowan_gif_spider/pipelines.py
@@ -197,7 +197,7 @@ def runGit(working_dir):
         commit_msg = '%s dwowan gif update [fileChanges=%d] [fileAdd=%d]' \
                             % (nowStr, len(diffObj), len(untracked_files))
         print (index.commit(commit_msg))
-        gitShell.commit('-am', commit_msg)
+        gitShell.commit('-am', '\"' + commit_msg + '\"')
 
         want_commit = True
 
